5135764cf53c54ad174aa818752875c4895abcf7
AndreiVed <catblues87@gmail.com>
2025-06-13 19:51:02 +0300

    solution

--- books/spiders/books_spider.py
+++ books/spiders/books_spider.py
@@ -1,7 +1,9 @@
-from typing import Generator
+from typing import Generator, re
 
 import scrapy
 from scrapy.http import Response
+
+from books.items import BooksItem
 
 
 class BooksSpiderSpider(scrapy.Spider):
@@ -17,27 +19,51 @@ class BooksSpiderSpider(scrapy.Spider):
     }
 
     def parse_detail_book(self, response: Response, **kwargs) -> Generator:
+        title = response.css("h1::text").get()
+        price = float(
+            response.css(".price_color::text").get().replace("£", "")
+        )
+        # amount_in_stock = int(
+        #         response.css(
+        #             "th:contains('Availability') + td::text"
+        #         ).get().split()[2].strip("(")
+        #     )
+        availability_text = response.css(
+            "th:contains('Availability') + td::text"
+        ).get()
+        amount_in_stock = None
+        if availability_text:
+            match = re.search(
+                r"\((\d+)\s*available\)",
+                availability_text,
+                re.IGNORECASE
+            )
+            if match:
+                amount_in_stock = int(match.group(1))
+
         raiting_str = response.css(
             "p.star-rating::attr(class)"
         ).get().split()[1]
         raiting = self.raiting_dict[raiting_str]
+        category = response.css(
+            ".breadcrumb li:nth-last-child(2) a::text"
+        ).get()
 
-        yield {
-            "title": response.css("h1::text").get(),
-            "price": float(
-                response.css(".price_color::text").get().replace("£", "")
-            ),
-            "amount_in_stock": int(
-                response.css(
-                    "th:contains('Availability') + td::text"
-                ).get().split()[2].strip("(")
-            ),
-            "rating": raiting,
-            "category": response.css(".breadcrumb li a::text").getall()[2],
-            "description": response.css(
-                "#product_description + p::text").get(),
-            "upc": response.css("th:contains('UPC') + td::text").get()
-        }
+        description = response.css(
+            "#product_description + p::text"
+        ).get()
+
+        upc = response.css("th:contains('UPC') + td::text").get()
+
+        yield BooksItem(
+            title=title,
+            price=price,
+            amount_in_stock=amount_in_stock,
+            raiting=raiting,
+            category=category,
+            description=description,
+            upc=upc
+        )
 
     def parse(self, response: Response, **kwargs) -> Generator:
         book_url_list = response.css(".product_pod a::attr(href)").getall()
